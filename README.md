a7821206e9203015fcf1de5fd2892fd53852dacd
ticki <ticki@users.noreply.github.com>
2016-07-02 15:42:18 +0200

    Update README

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Termion
 
-A pure Rust library for handling, manipulating and reading information about terminals. This provides a full-featured alternative to Termbox.
+Termion is a pure Rust, bindless library for low-level handling, manipulating
+and reading information about terminals. This provides a full-featured
+alternative to Termbox.
 
-Supports Redox and POSIX. Untested on Windows.
+Termion aims to be simple and yet expressive. It is bindless, meaning that it
+is not a front-end to some other library (e.g., ncurses or termbox), but a
+standalone library directly talking to the TTY.
+
+Supports Redox, Mac OS X, and Linux (or, in general, ANSI terminals).
 
 [Documentation.](http://ticki.github.io/termion/termion/) | [Examples.](https://github.com/Ticki/termion/tree/master/examples)
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,14 @@
-//! termion is a pure Rust library for reading, manipulating, and handling terminals.
+//! Termion is a pure Rust, bindless library for low-level handling, manipulating
+//! and reading information about terminals. This provides a full-featured
+//! alternative to Termbox.
 //!
-//! This crate is not stable, yet. However, if you do want stability, you should specify the
-//! revision (commit hash) in your `Cargo.toml`, this way builds are complete reproducible, and won't
-//! break.
+//! Termion aims to be simple and yet expressive. It is bindless, meaning that it
+//! is not a front-end to some other library (e.g., ncurses or termbox), but a
+//! standalone library directly talking to the TTY.
+//!
+//! Supports Redox, Mac OS X, and Linux (or, in general, ANSI terminals).
+//!
+//! For more information refer to the [README](https://github.com/ticki/termion).
 #![warn(missing_docs)]
 
 #![cfg_attr(feature = "nightly", feature(io))]
