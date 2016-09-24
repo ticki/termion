0d1025c532b06c59898908e3def509f63eaa3257
ticki <ticki@users.noreply.github.com>
2016-09-24 19:12:46 +0200

    Take a reference to the stream instead of the stream itself (in `is_tty`

--- src/tty.rs
+++ src/tty.rs
@@ -3,7 +3,7 @@ use std::os::unix::io::AsRawFd;
 
 /// Is this stream an TTY?
 #[cfg(not(target_os = "redox"))]
-pub fn is_tty<T: AsRawFd>(stream: T) -> bool {
+pub fn is_tty<T: AsRawFd>(stream: &T) -> bool {
     use libc;
 
     unsafe { libc::isatty(stream.as_raw_fd()) == 1}
@@ -11,7 +11,7 @@ pub fn is_tty<T: AsRawFd>(stream: T) -> bool {
 
 /// This will panic.
 #[cfg(target_os = "redox")]
-pub fn is_tty<T: AsRawFd>(_stream: T) -> bool {
+pub fn is_tty<T: AsRawFd>(_stream: &T) -> bool {
     unimplemented!();
 }
 
