ada64c4261c0566eec4febb56ab5068ccc5c38cb
Jonathan Strong <jonathan.strong@gmail.com>
2018-10-18 14:00:23 -0400

    moar colors

--- examples/color.rs
+++ examples/color.rs
@@ -1,10 +1,15 @@
 extern crate termion;
 
-use termion::{color, style};
+use termion::{color::{self, Fg}, style};
 
 fn main() {
     println!("{}Red", color::Fg(color::Red));
     println!("{}Blue", color::Fg(color::Blue));
     println!("{}Blue'n'Bold{}", style::Bold, style::Reset);
     println!("{}Just plain italic{}", style::Italic, style::Reset);
+
+    println!("{}", style::Reset);
+
+    println!("{}Yellow{}", color::Fg(color::Yellow), Fg(color::Reset));
+    println!("{}LightYellow{}", color::Fg(color::LightYellow), Fg(color::Reset));
 }
