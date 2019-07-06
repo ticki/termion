574f86367633fdc80e6f889f41eb24c27ca36f96
rabite0 <rabite@posteo.de>
2019-07-06 18:08:57 +0000

    Add terminal_size_pixels() to expose terminal's pixel size

--- src/sys/unix/size.rs
+++ src/sys/unix/size.rs
@@ -7,8 +7,8 @@ use super::libc::{c_ushort, ioctl, STDOUT_FILENO, TIOCGWINSZ};
 struct TermSize {
     row: c_ushort,
     col: c_ushort,
-    _x: c_ushort,
-    _y: c_ushort,
+    x: c_ushort,
+    y: c_ushort,
 }
 /// Get the size of the terminal.
 pub fn terminal_size() -> io::Result<(u16, u16)> {
@@ -16,5 +16,14 @@ pub fn terminal_size() -> io::Result<(u16, u16)> {
         let mut size: TermSize = mem::zeroed();
         cvt(ioctl(STDOUT_FILENO, TIOCGWINSZ.into(), &mut size as *mut _))?;
         Ok((size.col as u16, size.row as u16))
+    }
+}
+
+/// Get the size of the terminal, in pixels
+pub fn terminal_size_pixels() -> io::Result<(u16, u16)> {
+    unsafe {
+        let mut size: TermSize = mem::zeroed();
+        cvt(ioctl(STDOUT_FILENO, TIOCGWINSZ.into(), &mut size as *mut _))?;
+        Ok((size.x as u16, size.y as u16))
     }
 }
