52061a9b0e0672c57b669eef0b4fee506603fe6b
Jezza <jezzadabomb@gmail.com>
2020-03-30 00:13:52 +0200

    Fix formatting.

--- src/sys/windows/attr.rs
+++ src/sys/windows/attr.rs
@@ -1,37 +1,35 @@
 use std::{io, mem};
 
+use super::crossterm_winapi::{ConsoleMode, Handle};
 use super::Termios;
 
-use super::crossterm_winapi::{ConsoleMode, Handle};
-
 pub fn get_terminal_attr() -> io::Result<Termios> {
-	let console_mode = ConsoleMode::from(Handle::current_in_handle()?);
+    let console_mode = ConsoleMode::from(Handle::current_in_handle()?);
 
-	let mode = console_mode.mode()?;
+    let mode = console_mode.mode()?;
 
-	Ok(Termios(mode))
+    Ok(Termios(mode))
 }
 
 pub fn set_terminal_attr(termios: &Termios) -> io::Result<()> {
-	let console_mode = ConsoleMode::from(Handle::current_in_handle()?);
+    let console_mode = ConsoleMode::from(Handle::current_in_handle()?);
 
-	console_mode.set_mode(termios.0)?;
+    console_mode.set_mode(termios.0)?;
 
-	Ok(())
+    Ok(())
 }
 
 pub fn raw_terminal_attr(termios: &mut Termios) {
+    // These are copied from the MSDocs.
+    // Yes, technically, not the best, but Windows won't change these for obvious reasons.
+    // We could link in winapi explicitly, as crossterm_winapi is already doing that, but
+    // I feel it just adds a bit too much cruft, when we can just do this.
+    //
+    // https://docs.microsoft.com/en-us/windows/console/setconsolemode#parameters
+    const ENABLE_PROCESSED_INPUT: u32 = 0x0001;
+    const ENABLE_LINE_INPUT: u32 = 0x0002;
+    const ENABLE_ECHO_INPUT: u32 = 0x0004;
+    const RAW_MODE_MASK: u32 = ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT;
 
-	// These are copied from the MSDocs.
-	// Yes, technically, not the best, but Windows won't change these for obvious reasons.
-	// We could link in winapi explicitly, as crossterm_winapi is already doing that, but
-	// I feel it just adds a bit too much cruft, when we can just do this.
-	//
-	// https://docs.microsoft.com/en-us/windows/console/setconsolemode#parameters
-	const ENABLE_PROCESSED_INPUT: u32 = 0x0001;
-	const ENABLE_LINE_INPUT: u32 = 0x0002;
-	const ENABLE_ECHO_INPUT: u32 = 0x0004;
-	const RAW_MODE_MASK: u32 = ENABLE_LINE_INPUT | ENABLE_ECHO_INPUT | ENABLE_PROCESSED_INPUT;
-
-	termios.0 = termios.0 & !RAW_MODE_MASK;
+    termios.0 = termios.0 & !RAW_MODE_MASK;
 }
--- src/sys/windows/size.rs
+++ src/sys/windows/size.rs
@@ -4,10 +4,10 @@ use super::crossterm_winapi::ScreenBuffer;
 
 /// Get the size of the terminal.
 pub fn terminal_size() -> io::Result<(u16, u16)> {
-	let terminal_size = ScreenBuffer::current()?.info()?.terminal_size();
-	// windows starts counting at 0, unix at 1, add one to replicated unix behaviour.
-	Ok((
-		(terminal_size.width + 1) as u16,
-		(terminal_size.height + 1) as u16,
-	))
+    let terminal_size = ScreenBuffer::current()?.info()?.terminal_size();
+    // windows starts counting at 0, unix at 1, add one to replicated unix behaviour.
+    Ok((
+        (terminal_size.width + 1) as u16,
+        (terminal_size.height + 1) as u16,
+    ))
 }
