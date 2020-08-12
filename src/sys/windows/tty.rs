9506f31c992b181a8fe4e0ddb3dcacef3cc93643
Risto Kankkunen <risto.kankkunen@iki.fi>
2020-08-12 23:40:24 +0300

    Fix "termion::sys::tty::is_tty()".
    Previously the function returned true whether stdin or stdout was redirected
    or not.

--- src/sys/windows/tty.rs
+++ src/sys/windows/tty.rs
@@ -1,13 +1,11 @@
 use std::io::{self, Read, Result, Stdin, Stdout, Write};
 use std::os::windows::io::AsRawHandle;
+extern crate crossterm;
+use self::crossterm::tty::IsTty as _;
 
 /// Is this stream a TTY?
 pub fn is_tty<T: AsRawHandle>(stream: &T) -> bool {
-    // @MAYBE Jezza - 17 Dec. 2018: Is this the correct implementation?
-    // I just check against this program's stdin or stdout handle, and if they're the same, then the given
-    // handle must be a tty for something... I guess...
-    let raw = stream.as_raw_handle();
-    raw == io::stdin().as_raw_handle() || raw == io::stdout().as_raw_handle()
+    stream.is_tty()
 }
 
 /// Get the TTY device.
