7cf7c9ffae956982469fb94d17fc139c838477f4
Risto Kankkunen <risto.kankkunen@iki.fi>
2020-08-13 17:45:00 +0300

    Print mouse events in examples/mouse.rs.

--- examples/mouse.rs
+++ examples/mouse.rs
@@ -10,12 +10,13 @@ fn main() {
     let stdin = io::stdin();
     let mut stdout = MouseTerminal::from(io::stdout().into_raw_mode().unwrap());
 
-    writeln!(stdout,
-             "{}{}q to exit. Type stuff, use alt, click around...",
-             termion::clear::All,
-             termion::cursor::Goto(1, 1))
-            .unwrap();
-
+    writeln!(stdout, "{}", termion::clear::All).unwrap();
+    fn msg(out: &mut impl Write) {
+        writeln!(out, "{}q to exit. Type stuff, use alt, click around...{}",
+           termion::cursor::Goto(1, 1), termion::clear::UntilNewline).unwrap();
+    }
+    let (mut x, mut y) = (1, 2);
+    msg(&mut stdout);
     for c in stdin.events() {
         let evt = c.unwrap();
         match evt {
@@ -26,15 +27,17 @@ fn main() {
                     MouseEvent::Release(a, b) |
                     MouseEvent::Hold(a, b) => {
                         write!(stdout, "{}", cursor::Goto(a, b)).unwrap();
-                        let (x, y) = stdout.cursor_pos().unwrap();
-                        write!(stdout,
-                               "{}{}Cursor is at: ({},{}){}",
-                               cursor::Goto(5, 5),
+                        writeln!(stdout, "{}{}Cursor is at: ({},{}), event {:?}",
+                               cursor::Goto(x, y),
                                termion::clear::UntilNewline,
-                               x,
-                               y,
-                               cursor::Goto(a, b))
-                                .unwrap();
+                               a,
+                               b,
+                               me
+                        ).unwrap();
+                        let (x1, y1) = stdout.cursor_pos().unwrap();
+                        x = x1; y = y1;
+                        msg(&mut stdout);
+                        write!(stdout, "{}", cursor::Goto(a, b)).unwrap();
                     }
                 }
             }
