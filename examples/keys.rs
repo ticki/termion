83607fe3fbe34bf1bc0fd66da8d7f6c8a7954f8f
Risto Kankkunen <risto.kankkunen@iki.fi>
2020-08-13 17:28:46 +0300

    Print keys using Debug trait in examples/keys.rs.
    Leave the instructions on the screen, too.

--- examples/keys.rs
+++ examples/keys.rs
@@ -20,22 +20,13 @@ fn main() {
     for c in stdin.keys() {
         write!(stdout,
                "{}{}",
-               termion::cursor::Goto(1, 1),
+               termion::cursor::Goto(1, 2),
                termion::clear::CurrentLine)
                 .unwrap();
 
         match c.unwrap() {
             Key::Char('q') => break,
-            Key::Char(c) => println!("{}", c),
-            Key::Alt(c) => println!("^{}", c),
-            Key::Ctrl(c) => println!("*{}", c),
-            Key::Esc => println!("ESC"),
-            Key::Left => println!("←"),
-            Key::Right => println!("→"),
-            Key::Up => println!("↑"),
-            Key::Down => println!("↓"),
-            Key::Backspace => println!("×"),
-            _ => {}
+            ch@_ => writeln!(stdout, "[{:?}]", ch).unwrap(),
         }
         stdout.flush().unwrap();
     }
