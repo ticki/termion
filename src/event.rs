1948ddea8bb48758cbe9e2ba7c846a17dd923d91
Risto Kankkunen <risto.kankkunen@iki.fi>
2020-08-13 18:25:40 +0300

    Simplify pattern matching in "event::parse_csi()" with "ok()?".

--- src/event.rs
+++ src/event.rs
@@ -153,19 +153,19 @@ pub fn parse_event<I>(item: u8, iter: &mut I) -> Result<Event, Error>
 fn parse_csi<I>(iter: &mut I) -> Option<Event>
     where I: Iterator<Item = Result<u8, Error>>
 {
-    Some(match iter.next()? {
-        Ok(b'[') => match iter.next() {
-            Some(Ok(val @ b'A'..=b'E')) => Event::Key(Key::F(1 + val - b'A')),
+    Some(match iter.next()?.ok()? {
+        b'[' => match iter.next()?.ok()? {
+            val @ b'A'..=b'E' => Event::Key(Key::F(1 + val - b'A')),
             _ => return None,
         },
-        Ok(b'D') => Event::Key(Key::Left),
-        Ok(b'C') => Event::Key(Key::Right),
-        Ok(b'A') => Event::Key(Key::Up),
-        Ok(b'B') => Event::Key(Key::Down),
-        Ok(b'H') => Event::Key(Key::Home),
-        Ok(b'F') => Event::Key(Key::End),
-        Ok(b'Z') => Event::Key(Key::BackTab),
-        Ok(b'M') => {
+        b'D' => Event::Key(Key::Left),
+        b'C' => Event::Key(Key::Right),
+        b'A' => Event::Key(Key::Up),
+        b'B' => Event::Key(Key::Down),
+        b'H' => Event::Key(Key::Home),
+        b'F' => Event::Key(Key::End),
+        b'Z' => Event::Key(Key::BackTab),
+        b'M' => {
             // X10 emulation mouse encoding: ESC [ CB Cx Cy (6 characters only).
             let mut next = || iter.next().unwrap().unwrap();
 
@@ -193,7 +193,7 @@ fn parse_csi<I>(iter: &mut I) -> Option<Event>
                 _ => return None,
             })
         }
-        Ok(b'<') => {
+        b'<' => {
             // xterm mouse encoding:
             // ESC [ < Cb ; Cx ; Cy (;) (M or m)
             let mut buf = Vec::new();
@@ -244,7 +244,7 @@ fn parse_csi<I>(iter: &mut I) -> Option<Event>
 
             Event::Mouse(event)
         }
-        Ok(c @ b'0'..=b'9') => {
+        c @ b'0'..=b'9' => {
             // Numbered escape code.
             let mut buf = Vec::new();
             buf.push(c);
