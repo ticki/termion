4fafb44f8925a7f0df8e1f439f9b04d2593454c0
Luqman Aden <luqman@oxide.computer>
2021-09-27 07:43:32 -0700

    Use constants from libc instead of magic numbers.

--- src/sys/unix/attr.rs
+++ src/sys/unix/attr.rs
@@ -5,13 +5,13 @@ use super::{cvt, Termios};
 pub fn get_terminal_attr() -> io::Result<Termios> {
     unsafe {
         let mut termios = mem::zeroed();
-        cvt(libc::tcgetattr(1, &mut termios))?;
+        cvt(libc::tcgetattr(libc::STDOUT_FILENO, &mut termios))?;
         Ok(termios)
     }
 }
 
 pub fn set_terminal_attr(termios: &Termios) -> io::Result<()> {
-    cvt(unsafe { libc::tcsetattr(1, 0, termios) }).and(Ok(()))
+    cvt(unsafe { libc::tcsetattr(libc::STDOUT_FILENO, libc::TCSANOW, termios) }).and(Ok(()))
 }
 
 pub fn raw_terminal_attr(termios: &mut Termios) {
