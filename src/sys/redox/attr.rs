df913e86613e0fb44cd2dfb358db3baede73e9d3
Denis Varlakov <calm.lion0019@fastmail.com>
2024-01-04 14:34:13 +0100

    Fix compilation issue on redox

--- src/sys/redox/attr.rs
+++ src/sys/redox/attr.rs
@@ -1,3 +1,4 @@
+use std::convert::TryInto;
 use std::io;
 use std::os::fd::{AsRawFd, BorrowedFd};
 
@@ -6,7 +7,11 @@ use super::{cvt, Termios};
 pub fn get_terminal_attr(fd: BorrowedFd) -> io::Result<Termios> {
     let mut termios = Termios::default();
 
-    let fd = cvt(libredox::call::dup(fd.as_raw_fd(), b"termios"))?;
+    let fd: usize = fd
+        .as_raw_fd()
+        .try_into()
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
+    let fd = cvt(libredox::call::dup(fd, b"termios"))?;
     let res = cvt(libredox::call::read(fd, &mut termios));
     let _ = libredox::call::close(fd);
 
@@ -21,7 +26,11 @@ pub fn get_terminal_attr(fd: BorrowedFd) -> io::Result<Termios> {
 }
 
 pub fn set_terminal_attr(fd: BorrowedFd, termios: &Termios) -> io::Result<()> {
-    let fd = cvt(libredox::call::dup(fd.as_raw_fd(), b"termios"))?;
+    let fd: usize = fd
+        .as_raw_fd()
+        .try_into()
+        .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
+    let fd = cvt(libredox::call::dup(fd, b"termios"))?;
     let res = cvt(libredox::call::write(fd, termios));
     let _ = libredox::call::close(fd);
 
