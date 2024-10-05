179529d21eb78b684a9ce7c6318be15ab2516bd7
Andrey Turkin <andrey.turkin@gmail.com>
2024-10-05 19:46:16 +0300

    Get rid of now-unnecessary cvt helper

--- src/sys/redox/attr.rs
+++ src/sys/redox/attr.rs
@@ -2,7 +2,7 @@ use std::convert::TryInto;
 use std::io;
 use std::os::fd::{AsRawFd, BorrowedFd};
 
-use super::{cvt, Termios};
+use super::Termios;
 
 pub fn get_terminal_attr(fd: BorrowedFd) -> io::Result<Termios> {
     let mut termios = Termios::default();
@@ -11,8 +11,8 @@ pub fn get_terminal_attr(fd: BorrowedFd) -> io::Result<Termios> {
         .as_raw_fd()
         .try_into()
         .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
-    let fd = cvt(libredox::call::dup(fd, b"termios"))?;
-    let res = cvt(libredox::call::read(fd, &mut termios));
+    let fd = libredox::call::dup(fd, b"termios")?;
+    let res = libredox::call::read(fd, &mut termios);
     let _ = libredox::call::close(fd);
 
     if res? == termios.len() {
@@ -30,8 +30,8 @@ pub fn set_terminal_attr(fd: BorrowedFd, termios: &Termios) -> io::Result<()> {
         .as_raw_fd()
         .try_into()
         .map_err(|e| io::Error::new(io::ErrorKind::InvalidInput, e))?;
-    let fd = cvt(libredox::call::dup(fd, b"termios"))?;
-    let res = cvt(libredox::call::write(fd, termios));
+    let fd = libredox::call::dup(fd, b"termios")?;
+    let res = libredox::call::write(fd, termios);
     let _ = libredox::call::close(fd);
 
     if res? == termios.len() {
